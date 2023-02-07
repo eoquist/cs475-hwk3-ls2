799830b7cfc02b217198196e30b646eaa45ede2c
eoquist <emiwemi495@gmail.com>
2023-02-07 10:40:16 +0000

    going to bed

--- ls2.c
+++ ls2.c
@@ -10,23 +10,22 @@
 // TODO: function definitions here for ls2
 void ls2List(char *path, int indent)
 {
-    if (indent > 7)
-    {
-        return;
-    }
-
     DIR *currDir;
     struct dirent *entry;
     struct stat st;
-    stack_t *s = initstack(); // stack stores DIR and filenames
-
-    currDir = opendir(path); // Check to see if you can open directory
+    // char str[300]; 
+    // stack_t *s = initstack(); // stack stores DIR and filenames
+    
+    // Check to see if you can open directory
+    currDir = opendir(path); 
     if (currDir == NULL)
     {
-        printf("Can't open directory '%s' Exiting...\n", path);
+        // printf("Can't open directory '%s' Exiting...\n", path);
         exit(0); /* return to OS */
+        /*for some reason the tags dir doesnt exist in ls2Sol so idk why its causing me so much problems here :( */
     }
 
+    // Look at entries
     while ((entry = readdir(currDir)) != NULL)
     {
         if (entry->d_type == DT_DIR)
@@ -36,6 +35,19 @@ void ls2List(char *path, int indent)
                 continue; // don't want to get stuck in hell
             }
             printf("%*s%s/ (directory)\n", indent, "", entry->d_name);
+            // snprintf(str, sizeof(str),"%*s%s/ (directory)\n", indent, "", entry->d_name);
+            // push(s,str);
+            // str[0] = '\0';
+
+            /*
+                clowning !!!
+                need to make sure its not doing this depth first, essentially
+
+                push(s,str);
+            */
+            // Construct new path from our base path
+            strcat(path, "/");
+            strcat(path, entry->d_name);
             ls2List(path, indent + 4);
         }
         else
@@ -43,12 +55,15 @@ void ls2List(char *path, int indent)
             stat(entry->d_name, &st);
             off_t size = st.st_size;
 
-            printf("%*s%s (%ld bytes)\n", indent, "", entry->d_name, size); // tabbed file entries -- lld for off_t
+            printf("%*s%s (%ld bytes)\n", indent, "", entry->d_name, size);
+            // snprintf(str, sizeof(str),"%*s%s (%ld bytes)\n", indent, "", entry->d_name, size);
+            // push(s,str);
+            // str[0] = '\0';
         }
     }
 
-    printstack(s); // print stack
-    freestack(s);  // free up stack
+    // printstack(s); // print stack
+    // freestack(s);  // free up stack
 
     closedir(currDir); // Close DIR and exit
 }
@@ -57,32 +72,34 @@ void ls2Search(char *path, char *filenameToMatch)
 {
     DIR *currDir;
     struct dirent *entry;
+    // struct stat st;
+    stack_t *s = initstack(); // stack stores DIR and filenames
 
-    currDir = opendir(path); // Check to see if you can open directory
-    if (currDir != NULL)
+    // Check to see if you can open directory
+    currDir = opendir(path); 
+    if (currDir == NULL)
     {
-        printf("Can't open directory '%s'\n", path);
+        printf("Can't open directory '%s' Exiting...\n", path);
         exit(0); /* return to OS */
     }
 
+    // Look at entries
     while ((entry = readdir(currDir)) != NULL)
-    { // Look at each entry.
+    { 
         if (strcmp(entry->d_name, filenameToMatch) == 0)
         {
             printf("%s ", entry->d_name);
         }
     }
 
-    // if no path found
-    // printf("%s is not a directory path! Exiting...",pathname)
-    // then exit
-
     // if matching file found
     // print the directory names plus (directory) and then main with its size
     // if not found - no print required
 
-    // Close directory and exit.
-    closedir(currDir);
+    printstack(s); // print stack
+    freestack(s);  // free up stack
+
+    closedir(currDir); // Close DIR and exit
 }
 
 // malloc usage
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <dirent.h>
 #include "stack.h"
 #include "ls2.h"
 #include <unistd.h>
@@ -14,23 +15,19 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 
+	// Check to see if you can open directory
+	DIR *currDir;
+    currDir = opendir(argv[1]); 
+    if (currDir == NULL)
+    {
+        printf("Can't open directory '%s' Exiting...\n", argv[1]);
+        exit(0); /* return to OS */
+    }
+
 	if(argc == 2){
 		ls2List(argv[1],0);
 	}else if(argc == 3){
 		ls2Search(argv[1], argv[2]);
 	}
-
-	// // stack stores the lines to print out
-	// stack_t *s = initstack();
-
-	// push(s, "Hello1");
-	// push(s, "Hello2");
-	// push(s, "Hello3");
-
-	// // print stack
-	// printstack(s);
-
-	// // free up stack
-	// freestack(s);
 	return 0;
 }
